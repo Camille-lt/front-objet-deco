4525319a15a30b1366816d4e14dfc30baedebb10
Camille-lt <camillelebigot5@gmail.com>
2025-12-01 11:53:04 +0100

    updated filtertogle

--- component/FilterToggle.jsx
+++ component/FilterToggle.jsx
@@ -12,17 +12,17 @@ export default function FilterToggle({ onChange }) {
   };
 
   return (
-    <div className="rounded-lg pt-2 pb-2 pr-2 pl-2 flex max-w-xs mx-auto font-sans bg-gray-300/50 shadow-md">
+    <div className="rounded-lg pt-2 pb-2 pr-3 pl-2 space-x-1.5 flex max-w-xs mx-auto font-sans bg-gray-300/50 shadow-md mb-5">
       {FILTER_OPTIONS.map((filter) => {
         const isActive = activeFilter === filter;
         const containerClasses = isActive ? "bg-white shadow-xl" : "bg-transparent hover:bg-white/50";
-        const textClasses = isActive ? "text-gray-800 font-bold" : "text-gray-500 font-medium hover:text-gray-700";
+        const textClasses = isActive ? "text-gray-800 font-medium" : "text-gray-500 font-medium hover:text-gray-700";
 
         return (
           <div key={filter} className={`flex-1 rounded-lg transition-all duration-100 ${containerClasses}`}>
             <button
               onClick={() => handleToggle(filter)}
-              className={`w-full text-base ${textClasses}`}
+              className={`w-full text-base pl-2 pr-2  ${textClasses}`}
               style={{ backgroundColor: "transparent" }}
             >
               {filter}
