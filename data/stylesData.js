c63551abeeebea73f3399f6aed3f14c3afddcfe0
Camille-lt <camillelebigot5@gmail.com>
2025-12-01 11:56:00 +0100

    add data

---
+++ data/stylesData.js
@@ -0,0 +1,27 @@
+export const stylesData = [
+  {
+    id: 1,
+    name: "Scandinave",
+    image: "/styles/scandinave.jpg",
+  },
+  {
+    id: 2,
+    name: "Industriel",
+    image: "/styles/industriel.jpg",
+  },
+  {
+    id: 3,
+    name: "Bohème",
+    image: "/styles/boheme.jpg",
+  },
+  {
+    id: 4,
+    name: "Japandi",
+    image: "/styles/japandi.jpg",
+  },
+  {
+    id: 5,
+    name: "Mid-Century",
+    image: "/styles/midcentury.jpg",
+  },
+];
