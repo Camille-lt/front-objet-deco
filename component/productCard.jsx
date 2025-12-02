b532d268d546014f22c420cf345eff7e212489cb
Camille-lt <camillelebigot5@gmail.com>
2025-12-02 16:14:19 +0100

    updated component productCard

---
+++ component/productCard.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+export default function ProductCard({ produit }) {
+  return (
+    <div
+      className="
+        group
+        hover:shadow-lg transition-shadow duration-200
+        flex flex-col overflow-hidden
+        w-full min-h-[250px]
+        relative
+      "
+    >
+      {/* Image */}
+      <div className="w-full h-[200px] overflow-hidden relative">
+        <img
+          src={produit.image_url}
+          alt={produit.nom}
+          className="w-full h-full object-cover"
+        />
+
+        {/* Bouton Hover */}
+        <div
+          className="
+            absolute bottom-0 left-0 w-full
+            bg-black/40 text-white text-center
+            py-2 text-sm font-semibold
+            translate-y-full group-hover:translate-y-0
+            transition-all duration-300
+          "
+        >
+          Ajouter au panier
+        </div>
+      </div>
+
+      {/* Texte */}
+      <div className="p-3 pt-2 flex flex-col justify-between pb-10">
+        <h3 className="text-lg font-extrabold truncate">{produit.nom}</h3>
+        <p className="text-gray-500 text-xs line-clamp-2 h-8">
+          {produit.description}
+        </p>
+        <p className="text-black font-bold text-sm pt-1.5">
+          {produit.prix} €
+        </p>
+      </div>
+    </div>
+  );
+};
